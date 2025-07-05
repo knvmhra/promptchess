697197b1cf2706b17e478d973d78dea4daa417e4
Personal Work <kanav@themehra.com>
2025-07-05 01:13:09 -0700

    simple few shot

---
+++ model_player.py
@@ -0,0 +1,29 @@
+import chess
+import dspy
+import pickle
+
+with open('lichess_data.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+lm = dspy.LM(model= 'gpt-4.1')
+dspy.configure(lm=lm)
+
+class ChessPredictor(dspy.Signature):
+    board: str = dspy.InputField(
+        desc= "Chess position with ranks 1-8 (bottom to top) and files a-h (left to right).\
+        Uppercase pieces are white, Lowercase pieces are black. Castling rights, available en passant, and turn are provided.\
+        It is always your turn to play.")
+    move: str = dspy.OutputField(desc= "Best move in standard algebraic notation (SAN).")
+
+examples = [dspy.Example(board= pos, move= move) for pos, move, in data]
+
+predictor = dspy.ChainOfThought(ChessPredictor)
+
+def metric(example: dspy.Example, prediction: dspy.Prediction) --> bool:
+    return example.move == prediction.move
+
+optimizer = dspy.BootstrapFewShot(metric = metric, max_bootstrapped_demos= 90)
+
+predictor = optimizer.compile(predictor, trainset= examples)
+
+predictor.save('chess_predictor_optimized.json')
