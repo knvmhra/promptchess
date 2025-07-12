ba4c7e53f1b7a68da3abdae604ae7a76afff58d2
Personal Work <kanav@themehra.com>
2025-07-11 21:39:47 -0700

    not sure i like responses

--- simulate_games.py
+++ simulate_games.py
@@ -5,6 +5,8 @@ import random
 from model_player import ChessPredictor
 import dspy
 from datagen import stringify_board
+from openai import OpenAI, models
+import json
 
 class ChessPlayer(ABC):
     @abstractmethod
@@ -61,3 +63,59 @@ class DSPyPlayer(ChessPlayer):
     @property
     def name(self) -> str:
         return self._name
+
+class SimpleModelPlayer(ChessPlayer):
+    def __init__(self, model: str, instructions: str, max_retries: int):
+        self.client = OpenAI()
+        self.model = model
+        self.instructions = instructions
+        self.max_retries = max_retries
+
+    def get_move(self, board: chess.Board, reasoning: bool = False) -> chess.Move | Tuple[chess.Move, str]:
+        schema = {
+            "type": "object",
+            "properties": {
+                "chess_move_SAN": {"type": "string"},
+                **({"reasoning": {"type": "string"}} if reasoning else {})
+            },
+            "required": ["chess_move_SAN"] + (["reasoning"] if reasoning else []),
+            "additionalProperties": False
+        }
+
+        board_str = stringify_board(board)
+        failed_attempts = []
+
+        for i in range(self.max_retries):
+            if failed_attempts:
+                context = board_str + f"\nIllegal moves in this position:\n" + '\n'.join(failed_attempts)
+            else:
+                context = board_str
+
+
+            response = self.client.responses.create(
+                model = self.model,
+                input = context,
+                instructions = self.instructions,
+                text = {
+                    'format' : {
+                        'type': 'json_schema',
+                        'name': 'chess_player_schema',
+                        'schema' : schema,
+                        'strict' : True
+                    }
+                }
+            )
+            parsed_response = json.loads(response.output_text)
+
+            try:
+                move = board.parse_san(parsed_response['chess_move_SAN'])
+                if reasoning:
+                    return move, parsed_response['reasoning']
+                return move
+            except ValueError:
+                failed_attempts.append(parsed_response['chess_move_SAN'])
+
+        fallback_move = random.choice(list(board.legal_moves))
+        if reasoning:
+            return fallback_move, f"All {self.max_retries} attempts incorrect: {failed_attempts}."
+        return fallback_move
